5038103b9005e48117846557b14b0c9c52b367d7
David Vehlow <david.vehlow@fh-bielefeld.de>
2023-11-11 22:06:39 +0100

    Measure Points are now each loaded dynamically from a single json file. Coordinates from Measure Points are now being send to an OpenMeteoFetcher

--- web/js/measurePoints.js
+++ web/js/measurePoints.js
@@ -2,11 +2,45 @@ var measurePoints_options = {
     zoom: 18,
     plugins: new Map()
 };
+
 measurePoints_options.plugins.set('DataShowModal', {
     id: 'DataShowModal',
     active: true
 });
+
 window["DataShowModal_measurePoints_options"] = {
     attrsShown: ['latitude', 'longitude', 'name', 'description'],
     attrsFormat: new Map()
 };
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        let mapElement = document.querySelector('#measurePoints');   
+        // n = Anzahl mp-Dateien in data/openMeteoData
+        let n = 3;             
+        for (let i = 1; i <= n; i++) {
+            let filename = 'mp' + i + '.json';
+            let filepath = 'ref://openMeteoData/' + filename;
+            mapElement.swac_comp.addDataFromReference(filepath);
+
+            sendCoordinatesToAPI(filepath.replace('ref://', 'data/'));
+        }       
+    }, 1000); 
+});
+
+
+function sendCoordinatesToAPI(filepath) {
+    fetch(filepath)
+        .then(response => response.json())
+        .then(data => {
+            let latitude = data.latitude;
+            let longitude = data.longitude;
+               
+            processCoordinates(latitude, longitude);
+        })
+        .catch(error => {
+            console.error('Fehler beim Laden der JSON-Datei:', error);
+        });
+}
+
+
+
---
+++ web/js/openMeteoFetcher.js
@@ -0,0 +1,10 @@
+/* 
+ * This Javascript is supposed to fetch up-to-date-data from 
+ * openMeteo from given Coordinates
+ */
+
+function processCoordinates(latitude, longitude) {
+    console.log('Breitengrad:', latitude, 'Längengrad:', longitude);
+    // Hier kommt der Code zum senden an die API
+}
+
