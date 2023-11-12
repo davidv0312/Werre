676020c326a0ce260f6084269bb175925cb2db91
David Vehlow <david.vehlow@fh-bielefeld.de>
2023-11-12 14:52:40 +0100

    Logic for openMeteo communication added

--- web/js/measurePoints.js
+++ web/js/measurePoints.js
@@ -12,34 +12,45 @@ window["DataShowModal_measurePoints_options"] = {
     attrsShown: ['latitude', 'longitude', 'name', 'description'],
     attrsFormat: new Map()
 };
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        let mapElement = document.querySelector('#measurePoints');   
-        // n = Anzahl mp-Dateien in data/openMeteoData
-        let n = 3;             
+
+
+document.addEventListener('DOMContentLoaded', async function() { 
+    setTimeout(async function() { 
+        let map = document.querySelector('#measurePoints');   
+        let n = 3; // Anzahl mp-Dateien             
         for (let i = 1; i <= n; i++) {
             let filename = 'mp' + i + '.json';
-            let filepath = 'ref://openMeteoData/' + filename;
-            mapElement.swac_comp.addDataFromReference(filepath);
-
-            sendCoordinatesToAPI(filepath.replace('ref://', 'data/'));
+            let filepath = 'data/openMeteoData/' + filename;
+            
+            try {
+                let currentData = await sendCoordinatesToAPI(filepath);
+                if (currentData) {  
+                    
+                    map.swac_comp.addData(currentData); //HIER PROBLEM! Wie json objekt zur Karte hinzufügen?
+                    
+                    // map.swac_comp.addDataFromReference('ref://openMeteoData/' + filename); // DAS HIER GEHT!
+                    
+                    console.log('x = ' + currentData.latitude + '| y = ' + currentData.longitude);
+                }
+            } catch (error) {
+                console.error('Fehler:', error);
+            }            
         }       
     }, 1000); 
 });
 
 
-function sendCoordinatesToAPI(filepath) {
-    fetch(filepath)
-        .then(response => response.json())
-        .then(data => {
-            let latitude = data.latitude;
-            let longitude = data.longitude;
-               
-            processCoordinates(latitude, longitude);
-        })
-        .catch(error => {
-            console.error('Fehler beim Laden der JSON-Datei:', error);
-        });
+async function sendCoordinatesToAPI(filepath) {
+    try {
+        let response = await fetch(filepath);
+        let data = await response.json();
+        let latitude = data.latitude;
+        let longitude = data.longitude;
+
+        return await processCoordinatesToOpenMeteoRequest(latitude, longitude);
+    } catch (error) {
+        console.error('Fehler beim Laden der JSON-Datei:', error);
+    }
 }
 
 
--- web/js/openMeteoFetcher.js
+++ web/js/openMeteoFetcher.js
@@ -3,8 +3,38 @@
  * openMeteo from given Coordinates
  */
 
-function processCoordinates(latitude, longitude) {
-    console.log('Breitengrad:', latitude, 'Längengrad:', longitude);
-    // Hier kommt der Code zum senden an die API
+function getCurrentDateFormatted() {
+    let currentDate = new Date();
+    let year = currentDate.getFullYear();
+    let month = currentDate.getMonth() + 1; 
+    let day = currentDate.getDate();
+
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+
+    return year + '-' + month + '-' + day;
 }
+
+
+async function processCoordinatesToOpenMeteoRequest(latitude, longitude) {
+    let startDate = '2023-10-01';
+    // let currentDate = getCurrentDateFormatted();
+    let currentDate = '2023-11-01';
+
+    let url = `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=${startDate}&end_date=${currentDate}&hourly=temperature_2m,relative_humidity_2m,soil_temperature_0_to_7cm,soil_temperature_7_to_28cm,soil_temperature_28_to_100cm&timezone=Europe%2FBerlin`;
+
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await response.json();
+        console.log('Empfangene Daten:', data);
+        return data; 
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+    }
+}
+
+
 
