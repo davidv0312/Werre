f948edc8d70b2d67a6f80a4aa35cf0e8e838e562
David Vehlow <david.vehlow@fh-bielefeld.de>
2023-12-11 13:12:37 +0100

    added coloring for polygons

--- web/js/heatmap.js
+++ web/js/heatmap.js
@@ -1,13 +1,81 @@
-polygonNumber = 21;
 
-for (let i = 0; i < polygonNumber; i++) {
-    measurePoints_options.modelFiles.push(
-            {
-                url: '/Werre/data/polygons/polygon'+ i.toString() +'.geojson',
-                name: 'polygon_' + i.toString(),
-                fillColor: '0x67ADDFFF',
-                outlineColor: 'blue',
-                outlineWidth: 2
-            }
-    );
+
+function getColorForTemperature(temperature) {
+  let closestTemp = null;
+  let minDiff = Infinity;
+
+  for (const tempKey in tempToColor) {
+    const temp = parseInt(tempKey);
+    const diff = Math.abs(temperature - temp);
+
+    if (diff < minDiff) {
+      minDiff = diff;
+      closestTemp = temp;
+    }
+  }
+
+  return tempToColor[closestTemp.toString()].toString(16);
 }
+
+async function getPolygonColor(index) {
+    
+    let polygon = await loadPolygon(index);
+    let coordinates = polygon.features[0].geometry.coordinates[0];
+    const x1 = coordinates[0][0]; 
+    const y1 = coordinates[0][1]; 
+    const x2 = coordinates[1][0]; 
+    const y2 = coordinates[1][1]; 
+    const x3 = coordinates[2][0]; 
+    const y3 = coordinates[2][1]; 
+    const weather1 = await findMatchingMeasurePointWeatherData(x1, y1);
+    const weather2 = await findMatchingMeasurePointWeatherData(x2, y2);
+    const weather3 = await findMatchingMeasurePointWeatherData(x3, y3);      
+    console.log('Heatmap Polygon ' + index + ' :' + weather1.temp + '|' + weather2.temp + '|' + weather3.temp);
+
+    return getColorForTemperature((weather1.temp+weather2.temp+weather3.temp)/3);
+    
+}
+
+async function loadPolygon(i) {
+    let polygon;
+    const path = 'data/polygons/polygon';
+    
+    try {
+        const response = await fetch(`${path}${i}.geojson`);
+        if (!response.ok) {
+            throw new Error(`Fehler beim Laden von ${path}${i}.geojson: ${response.status}`);
+        }
+        const data = await response.json();
+        polygon = data;
+    } catch (error) {
+        console.error('Fehler beim Laden eines Polygons in der Heatmap:', error);
+    }  
+
+    return polygon;
+}
+
+async function addHeatmap() {
+    polygonNumber = 21;
+    for (let i = 0; i < polygonNumber; i++) {
+        
+        let color;  
+
+        color = '#' + await getPolygonColor(i);
+        console.log('Heatmap Polygon ' + i + 'farbe: ' + color);
+        let modelFile = {
+            url: '/Werre/data/polygons/polygon'+ i.toString()+ '.geojson',
+            name: 'polygon_' + i.toString(),
+            fillColor: color,
+            outlineWidth: 2            
+        };  
+        document.querySelector('#measurePoints').swac_comp.loadModelFile(modelFile);
+    }
+}
+
+setTimeout(async function () {
+    addHeatmap();
+}, 3000 );
+
+
+
+
